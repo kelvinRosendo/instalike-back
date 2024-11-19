acfc093698d9760194e833def5a5762778cec5f0
kelvin78226 <rosendokelvin8@gmail.com>
2024-11-19 18:51:47 -0300

    entendendo a função get(), do nodejs e testando o servidor via localhost

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
- 
+ app.get("/", (req, res) => {
+    res.status(200).send("Texto enviado pela rota 200, bem vindo ao servidor!");
+ });
+
+
